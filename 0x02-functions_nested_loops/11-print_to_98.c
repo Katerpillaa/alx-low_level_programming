a9efd8dce23d4573c2a38071f14f26ebf6fc539e
katerpillaa <katlegodavid72@gmail.com>
2023-02-21 10:45:03 -0500

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -add two values
+ * @n1: The first number to be computed
+ * @n2: the second number to be computed
+ * Return: Interger Value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
